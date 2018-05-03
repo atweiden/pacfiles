698f8fc647f280e4f453a4b552071f5a8efd5d05
Andy Weidenbaum <archbaum@gmail.com>
2018-05-03 14:27:54 -0400

    rm vim markers

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,6 @@
 # usage
 # -----------------------------------------------------------------------------
 
-# _usage_function() {{{
-
 _usage_function() {
 read -r -d '' _usage_string <<'EOF'
 Usage:
@@ -29,8 +27,6 @@ Options:
 EOF
 echo "$_usage_string"
 }
-
-# end _usage_function() }}}
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
