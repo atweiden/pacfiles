74990e8ace9c9568e41a605213bc0f35925cd7a9
Andy Weidenbaum <archbaum@gmail.com>
2018-05-30 09:23:23 -0400

    clean up bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -143,11 +143,9 @@ rsync --verbose "${_rsync_opts[@]}" "$DIR/" "$HOME"
 # vim-plug
 # -----------------------------------------------------------------------------
 
-if ! [[ -e ~/.vim/autoload/plug.vim ]]; then
-  curl \
-    -fLo ~/.vim/autoload/plug.vim \
-    --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+_plug_url='https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+if ! [[ -e "$HOME/.vim/autoload/plug.vim" ]]; then
+  curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs "$_plug_url"
   vim +PlugInstall +qa
 fi
 
@@ -156,9 +154,11 @@ fi
 # neovim
 # -----------------------------------------------------------------------------
 
-mkdir -p ~/.config
-! [[ -d ~/.config/nvim ]] && ln -s ~/.vim ~/.config/nvim
-! [[ -e ~/.config/nvim/init.vim ]] && ln -s ~/.vimrc ~/.config/nvim/init.vim
+mkdir -p "$HOME/.config"
+! [[ -d "$HOME/.config/nvim" ]] \
+  && ln -s "$HOME/.vim" "$HOME/.config/nvim"
+! [[ -e "$HOME/.config/nvim/init.vim" ]] \
+  && ln -s "$HOME/.vimrc" "$HOME/.config/nvim/init.vim"
 
 
 # -----------------------------------------------------------------------------
@@ -191,10 +191,8 @@ sed -i "s#youremail#$email#"       "$HOME/.hgrc"
 # redshift
 # -----------------------------------------------------------------------------
 
-sed -i \
-  -e "s#LATITUDE#$latitude#" \
-  -e "s#LONGITUDE#$longitude#" \
-  "$HOME/.config/redshift/redshift.conf"
+sed -i "s#LATITUDE#$latitude#"     "$HOME/.config/redshift/redshift.conf"
+sed -i "s#LONGITUDE#$longitude#"   "$HOME/.config/redshift/redshift.conf"
 
 
 # -----------------------------------------------------------------------------
