5c7030488ec877fc2ca9fe182418629407319dd4
atweiden <atweiden@tutanota.de>
2020-07-04 08:47:37 +1000

    exclude .gitkeep files during bootstrap rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -130,6 +130,7 @@ _rsync_opts=()
 
 # exclude files
 _rsync_opts+=('--exclude=.git'
+              '--exclude=.gitkeep'
               '--exclude=.hg'
               '--exclude=.subgit'
               '--exclude=doc'
