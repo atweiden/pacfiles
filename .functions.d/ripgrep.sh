0591f14c4dc71ef55136998f16c7caf04de516f8
Andy Weidenbaum <atweiden@ioiojo.com>
2022-08-29 17:16:06 +1000

    add bash function rgs for ripgrepping void srcpkgs

---
+++ .functions.d/ripgrep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# rgs: search templates in void-linux/void-packages
+# -----------------------------------------------------------------------------
+
+rgs() { rg --glob template --regexp "$@" srcpkgs/; }
+
+# vim: set filetype=sh foldmethod=marker foldlevel=0 nowrap:
